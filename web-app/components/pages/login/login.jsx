9ef82982b8fab77213a17000f846bb1d07602622
MIsterFathur <04fathur.rahman@gmail.com>
2024-09-06 14:47:56 +0700

    using box component

--- web-app/components/pages/login/login.jsx
+++ web-app/components/pages/login/login.jsx
@@ -1,5 +1,6 @@
+import { Box } from '@mui/material';
 import React from 'react';
 
 export const Login = () => {
-    return <div>Assalamualaikum, saya login</div>;
+    return <Box margin="24px">Assalamualaikum, saya login</Box>;
 };
