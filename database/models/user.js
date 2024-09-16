b9bfb99e4458b37683f1c1e9d2c78231e088db0b
abualif1413 <ilhamakbar_90@hotmail.com>
2024-09-17 02:06:05 +0700

    SCRUM-2 | use this as current model reference

--- database/models/user.js
+++ database/models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
-            User.belongsTo(models.RoleUser, {
+            this.belongsTo(models.RoleUser, {
                 foreignKey: 'roleUserId',
             });
         }
